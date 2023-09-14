66effca4ae9d82df7e584611840b308c2fa1e1aa
KMmiau <143568357+KMmiau@users.noreply.github.com>
2023-09-13 18:44:46 -0700

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES/src/eva1_10_identificadores/EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,25 @@
+package eva1_10_identificadores;
+
+public class EVA1_10_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //int 1salario; no cumple las reglas: inicia con numero
+        //int salario&; no cumple: & no es caracter alfanumerico
+        int mi_salario;
+        int $salario;
+        int salario1000;
+        //int _; no es valido
+        int $;
+        //int mi salario; no valido: espacio en blanco
+        
+        int salario;
+        int Salario;
+        int SALARIO;
+        //int salario; no podemos repetir identificadores
+        
+        //int public; no podemos usar keywords (palabras reservadas)
+        
+        
+    }
+    
+}
